53bf0a7520b4a8e0bce406d39bf394bb5e2e9ffd
Nicholas Bergantz <nicholas.bergantz@gmail.com>
2025-06-25 13:06:00 -0700

    locking this for now.

--- Package.swift
+++ Package.swift
@@ -16,14 +16,14 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/keyvariable/kvSIMD.swift.git", from: "1.1.0"),
-        // .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
     ],
     targets: [
         .executableTarget(
             name: "SwiftRoboticVisualizer",
             dependencies: [
                 "SwiftRobotics",
-                // .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
             ],
             path: "Sources/SwiftRoboticVisualizer"
         ),
